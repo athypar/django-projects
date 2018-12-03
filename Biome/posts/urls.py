05e92f2bb8293fb17b717ee51ff73e3fefaa4e33
athypar <45423201+athypar@users.noreply.github.com>
2018-12-03 08:36:58 -0600

    commit

--- Biome/posts/urls.py
+++ Biome/posts/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     url(r"^$", views.PostList.as_view(), name="all"),
     url(r"new/$", views.CreatePost.as_view(), name="create"),
     url(r"by/(?P<username>[-\w]+)/$",views.UserPosts.as_view(),name="for_user"),
-    path("by/(?P<username>[-\w]+)/(?P<pk>\d+)/",views.PostDetail.as_view(),name="single"),
+    url(r"by/(?P<username>[-\w]+)/(?P<pk>\d+)/$",views.PostDetail.as_view(),name="single"),
     url(r"^posts/in/(?P<slug>[-\w]+)/$",views.SingleGroup.as_view(),name="single1"),
     url(r"delete/(?P<pk>\d+)/$",views.DeletePost.as_view(),name="delete"),
 ]
